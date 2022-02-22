ff4cd6ac650eace710c65412042ccc11e0837a0e
gerardohb99 <gerardohb99@gmail.com>
2022-02-23 00:33:35 +0100

    cambios de nombres

--- executeTest.bash
+++ executeTest.bash
@@ -3,7 +3,7 @@ python busters.py -l extra3.lay -p BasicAgentAA  -t 0
 python busters.py -l extra3.lay -p BasicAgentAA -t 0
 python busters.py -l extra3.lay -p BasicAgentAA -t 0
 python busters.py -l extra3.lay -p BasicAgentAA -t 0
-wc -l output.txt > salida-extra2.txt
+wc -l output.txt > salida-extra3.txt
 
 rm output.txt
 python busters.py -l classic.lay -p BasicAgentAA -t 0
@@ -17,7 +17,7 @@ python busters.py -l extra3.lay -p BasicAgentAA -g RandomGhost -t 0
 python busters.py -l extra3.lay -p BasicAgentAA -g RandomGhost -t 0
 python busters.py -l extra3.lay -p BasicAgentAA -g RandomGhost -t 0
 python busters.py -l extra3.lay -p BasicAgentAA -g RandomGhost -t 0
-wc -l output.txt > salida-extra2-random.txt
+wc -l output.txt > salida-extra3-random.txt
 
 rm output.txt
 python busters.py -l classic.lay -p BasicAgentAA -g RandomGhost -t 0
