3cebe267d7bdb641c7e1fade740e1b574f08c925
gerardohb99 <gerardohb99@gmail.com>
2022-02-23 00:10:09 +0100

    test listo

--- executeTest.bash
+++ executeTest.bash
@@ -1,16 +1,27 @@
-
 rm output.txt
-python busters.py -l extra3.lay -p BasicAgentAA
+python busters.py -l extra3.lay -p BasicAgentAA  -t 0
+python busters.py -l extra3.lay -p BasicAgentAA -t 0
+python busters.py -l extra3.lay -p BasicAgentAA -t 0
+python busters.py -l extra3.lay -p BasicAgentAA -t 0
 wc -l output.txt > salida-extra2.txt
 
-# rm output.txt
-# python busters.py -l classic.lay -p BasicAgentAA
-# wc -l output.txt > salida-classic.txt
+rm output.txt
+python busters.py -l classic.lay -p BasicAgentAA -t 0
+python busters.py -l classic.lay -p BasicAgentAA -t 0
+python busters.py -l classic.lay -p BasicAgentAA -t 0
+python busters.py -l classic.lay -p BasicAgentAA -t 0
+wc -l output.txt > salida-classic.txt
 
-# rm output.txt
-# python busters.py -l extra3.lay -p BasicAgentAA -g RandomGhost
-# wc -l output.txt > salida-extra2.txt
+rm output.txt
+python busters.py -l extra3.lay -p BasicAgentAA -g RandomGhost -t 0
+python busters.py -l extra3.lay -p BasicAgentAA -g RandomGhost -t 0
+python busters.py -l extra3.lay -p BasicAgentAA -g RandomGhost -t 0
+python busters.py -l extra3.lay -p BasicAgentAA -g RandomGhost -t 0
+wc -l output.txt > salida-extra2-random.txt
 
-# rm output.txt
-# python busters.py -l classic.lay -p BasicAgentAA -g RandomGhost
-# wc -l output.txt > salida-classic.txt
+rm output.txt
+python busters.py -l classic.lay -p BasicAgentAA -g RandomGhost -t 0
+python busters.py -l classic.lay -p BasicAgentAA -g RandomGhost -t 0
+python busters.py -l classic.lay -p BasicAgentAA -g RandomGhost -t 0
+python busters.py -l classic.lay -p BasicAgentAA -g RandomGhost -t 0
+wc -l output.txt > salida-classic-random.txt
